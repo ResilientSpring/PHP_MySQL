19de3ad35442a9b8210a28a1e877238f1b17a505
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-09-02 15:24:41 +0800

    Committed 2021/09/02

---
+++ PHP - A Beginner's Guide/Chapter1 - Introducing PHP/Single quote versus double quotes.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+    <title>' ' versus " "</title>
+</head>
+<body>
+
+<?php
+// output:
+// Welcome
+// to
+// PHP
+echo "Welcome\nto\nPHP";
+?>
+
+<?php
+// output: Welcome\nto\nPHP
+echo 'Welcome\to\nPHP';
+?>
+
+</body>
+</html>
