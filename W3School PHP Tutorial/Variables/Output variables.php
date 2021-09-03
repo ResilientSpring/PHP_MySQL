18b95b7c301416757318f2ea11188cfa0445577f
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-09-03 17:17:41 +0800

    Committed 2021/09/03

---
+++ W3School PHP Tutorial/Variables/Output variables.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+    <title>
+        Output variables
+    </title>
+</head>
+<body>
+The PHP <span style="background-color: #cccccc; color: red">echo</span>
+statement is used to output/print data to/on the screen.
+
+<?php
+$text = "W3Schools.com";
+echo "I love $text!";
+?>
+</body>
+</html>
