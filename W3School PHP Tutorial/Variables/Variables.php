1e4d30b5e817cf7c6d03a48fe574f2dd58e8c1fc
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-09-03 17:06:06 +0800

    Committed 2021/09/03

---
+++ W3School PHP Tutorial/Variables/Variables.php
@@ -0,0 +1,29 @@
+<html>
+<head>
+    <title>
+        Creating/declaring variables
+    </title>
+</head>
+<body>
+<p>Variables are "containers" for storing information.</p>
+
+<h2>Creating/declaring PHP variables</h2>
+<p>
+    In PHP, variables starts with the <span style="background-color: darkgrey; color: red">$</span>
+    sign, followed by the name of the variable.
+</p>
+
+<?php
+$txt = "Hello World!";
+$x = 5;
+$y = 10.5;
+
+echo $txt;
+echo "<br/>";
+echo $x;
+echo "<br>";
+echo $y;
+?>
+
+</body>
+</html>
