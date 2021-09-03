b44f7add3f6aa9630bc7687a10632b4860d3e04c
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-09-03 19:06:26 +0800

    Committed 2021/09/03

---
+++ PHP - A Beginner's Guide/Chapter3 - Controlling Program Flow/The if statement.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+    <title>
+        The if statement
+    </title>
+</head>
+<body>
+
+<?php
+// if number is less than zero.
+// print message
+$number = - 88;
+if ($number < 0)
+    echo "That number is negative.";
+?>
+</body>
+</html>
