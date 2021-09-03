b1d63515eed09081336c0cbcec08f31f41b5674b
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-09-03 17:19:01 +0800

    Committed 2021/09/03

--- W3School PHP Tutorial/Variables/Output variables.php
+++ W3School PHP Tutorial/Variables/Output variables.php
@@ -8,6 +8,8 @@
 The PHP <span style="background-color: #cccccc; color: red">echo</span>
 statement is used to output/print data to/on the screen.
 
+<br/>
+
 <?php
 $text = "W3Schools.com";
 echo "I love $text!";
