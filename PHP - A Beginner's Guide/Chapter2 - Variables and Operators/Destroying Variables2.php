5d0068d6489decaee8cd9896e05d56acd109cec4
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-09-03 20:21:28 +0800

    Committed 2021/09/03

--- PHP - A Beginner's Guide/Chapter2 - Variables and Operators/Destroying Variables2.php
+++ PHP - A Beginner's Guide/Chapter2 - Variables and Operators/Destroying Variables2.php
@@ -4,6 +4,22 @@
 </head>
 <body>
 
+<?php
+// assign value to variable
+$car = "Porsche";
 
+// print variable value
+// output: "Before unset(), my car is a Porsche"
+echo "Before unset(), my car is a $car";
+
+// assign empty value to variable.
+$car = null;
+
+echo "\n";
+
+// print variable value
+// output: "After $car = null, my car is a "
+echo "After \$car = null, my car is a $car";
+?>
 </body>
 </html>
