d580365323c4b9eb3cd53696160a8196d7f55c32
Ceiling_roof <ted10014@hotmail.com>
2021-09-12 12:00:50 +0800

    Committed 2021/09/12

--- PHP - A Beginner's Guide/Chapter2 - Variables and Operators/Variable data types.php
+++ PHP - A Beginner's Guide/Chapter2 - Variables and Operators/Variable data types.php
@@ -5,7 +5,23 @@
 <body>
 
 <?php
+// define string variable
+$whoami = 'Sarah';
 
+// output: 'string'
+echo gettype($whoami);
+
+// assign new integer value to variable
+$whoami = 99.8;
+
+// output: 'double'
+echo gettype($whoami);
+
+// empty variable
+$whoami = null;
+
+// output" 'NULL'
+echo gettype($whoami);
 ?>
 </body>
 </html>
