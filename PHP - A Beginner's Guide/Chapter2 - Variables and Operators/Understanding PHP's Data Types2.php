5bcb4c3eb2bb5bf2cd486e6bd70f81a65e493d0b
Ceiling_roof <ted10014@hotmail.com>
2021-09-12 11:24:47 +0800

    Committed 2021/09/12

--- PHP - A Beginner's Guide/Chapter2 - Variables and Operators/Understanding PHP's Data Types2.php
+++ PHP - A Beginner's Guide/Chapter2 - Variables and Operators/Understanding PHP's Data Types2.php
@@ -7,7 +7,14 @@
 <body>
 
 <?php
+// 8, specified as octal value.
+$a = 010;
 
+// 1500, specified as hexadecimal value.
+$b = 0x5dc;
+
+// 690, in scientific notation.
+$c = 6.9E+2;
 ?>
 </body>
 </html>
