93ad0203aed55df4c0b3306ed1f3ddd8cb7e279f
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-10-04 22:34:34 +0800

    Committed 2021/10/04

---
+++ Web Database Applications with PHP and MySQL/Chapter2 - The PHP Scripting Language/Outputting data with echo and print.php
@@ -0,0 +1,15 @@
+<?php
+
+print "Hello, world" . "<br>";
+
+// echo works just the same
+echo "Hello, world" . "<br>";
+
+// numbers can be printed with echo too
+echo 123, "<br>";
+
+print 456;
+
+// So can the contents of variables
+$outputString = "Hi!";
+echo $outputString;
