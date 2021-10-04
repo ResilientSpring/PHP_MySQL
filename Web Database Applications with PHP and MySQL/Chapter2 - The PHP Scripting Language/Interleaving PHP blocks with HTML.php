4545fe0c75340b2037969232970cd60c18b87f0c
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-10-04 22:26:18 +0800

    Committed 2021/10/04

---
+++ Web Database Applications with PHP and MySQL/Chapter2 - The PHP Scripting Language/Interleaving PHP blocks with HTML.php
@@ -0,0 +1,22 @@
+<?php
+$outputString = "Hello, world";
+
+?>
+
+<html>
+<head>
+    <title>
+        <?php
+        print $outputString;
+        ?>
+    </title>
+</head>
+
+<body bgcolor="#ffffff">
+<h1>
+    <?php
+    print $outputString;
+    ?>
+</h1>
+</body>
+</html>
