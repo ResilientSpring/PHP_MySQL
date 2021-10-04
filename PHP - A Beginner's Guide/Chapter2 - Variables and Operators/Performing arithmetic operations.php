44ecd0a5cc8baa398c9e351a1afae22b3f5b3952
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-10-04 11:29:28 +0800

    Committed 2021/10/04

--- PHP - A Beginner's Guide/Chapter2 - Variables and Operators/Performing arithmetic operations.php
+++ PHP - A Beginner's Guide/Chapter2 - Variables and Operators/Performing arithmetic operations.php
@@ -1,1 +1,22 @@
+<html>
+<head>
+    <title>
+        Arithmetic operations
+    </title>
+</head>
+<body>
+
 <?php
+// define variables
+$x = 10;
+$y = 5;
+$z = 3;
+
+// add
+$sum = $x + $y;
+
+echo "$x + $y = $sum\n";
+
+?>
+</body>
+</html>
