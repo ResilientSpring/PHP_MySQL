d359d1ca9dad9e10bbba3cabb4d2ba932916fd71
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-10-05 10:51:04 +0800

    Committed 2021/10/05

--- Web Database Applications with PHP and MySQL/Chapter2 - The PHP Scripting Language/String literals.php
+++ Web Database Applications with PHP and MySQL/Chapter2 - The PHP Scripting Language/String literals.php
@@ -3,4 +3,11 @@
 print 'This works';
 print "\n";
 print '<br>';
-print "Just like this.";
+print "Just like this.\n";
+
+$var = "The quick brown fox 
+        jumps over the lazy dog.";
+
+print $var;
+
+# $query = "SELECT max(order_id) FROM orders where cust_id = $custID";
