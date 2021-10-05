0426b241b1a6d9df711e0ea927cf1e43daaa6aac
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-10-05 11:49:58 +0800

    Committed 2021/10/05

--- Web Database Applications with PHP and MySQL/Chapter2 - The PHP Scripting Language/Variable substitution.php
+++ Web Database Applications with PHP and MySQL/Chapter2 - The PHP Scripting Language/Variable substitution.php
@@ -17,3 +17,14 @@ print $message;
 $message2 = "This \$vehicle holds \$number people.";
 
 print $message2;
+
+// When the name of the variable is ambiguous, brace{} can delimit the name.
+$memory = 256;
+
+$message3 = "My computer has $memory Mbytes of RAM.";
+
+print "\n <br>$message3";
+
+$message4 = "My computer has {$memory}Mbytes of RAM.";
+
+print "\n$message4";
