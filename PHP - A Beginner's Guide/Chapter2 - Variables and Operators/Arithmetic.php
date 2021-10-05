efb87883cb65d64b2e45067807cd6783c670acc9
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-10-05 12:22:38 +0800

    Committed 2021/10/05

---
+++ PHP - A Beginner's Guide/Chapter2 - Variables and Operators/Arithmetic.php
@@ -0,0 +1,21 @@
+<?php
+
+$x = 10;
+$y = 5;
+$z = 3;
+
+$sum = $x + $y;
+
+echo "$x + $y = $sum\n";
+
+$diff = $x - $y;
+
+echo "$x - $y = $diff\n";
+
+$product = $x * $y;
+
+echo "$x * $y = $product\n";
+
+$quotient = $x / $y;
+
+echo "$x / $y = $quotient";
