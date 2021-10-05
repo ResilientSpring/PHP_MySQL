ecaee07cfedc06456320c62168fe240ceebfa32d
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-10-05 11:02:13 +0800

    Committed 2021/10/05

--- Web Database Applications with PHP and MySQL/Chapter2 - The PHP Scripting Language/Variable substitution.php
+++ Web Database Applications with PHP and MySQL/Chapter2 - The PHP Scripting Language/Variable substitution.php
@@ -1,1 +1,13 @@
 <?php
+
+/* Variable substitution provides a convenient way to embed data held in a variable directly into
+string literals.
+
+PHP examines, or parses, double-quoted strings and replaces variable names with the variable's value.
+ * */
+
+$number = 45;
+$vehicle = "bus";
+$message = "This $vehicle holds $number people.";
+
+print $message;
