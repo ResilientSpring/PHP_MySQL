7bf8a60812ab6579cd1fd12e500a3adb32cbdc5c
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-10-29 11:47:18 +0800

    Committed 2021/10/29

--- Web Database Applications with PHP and MySQL/Chapter2 - The PHP Scripting Language/Constants.php
+++ Web Database Applications with PHP and MySQL/Chapter2 - The PHP Scripting Language/Constants.php
@@ -3,4 +3,5 @@
 // Unless you need any type of conditional or expression definition, use consts instead of define()
 // Reference: https://stackoverflow.com/a/3193704/
 
-const PI = 3.14159;
+const PI = 3.14159;  // Constants are not preceded by a $ character.
+
