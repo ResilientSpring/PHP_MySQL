d538a2649ca5385c1d31ab5424b6c9ed9291a958
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-10-29 11:45:55 +0800

    Committed 2021/10/29

---
+++ Web Database Applications with PHP and MySQL/Chapter2 - The PHP Scripting Language/Constants.php
@@ -0,0 +1,6 @@
+<?php
+
+// Unless you need any type of conditional or expression definition, use consts instead of define()
+// Reference: https://stackoverflow.com/a/3193704/
+
+const PI = 3.14159;
