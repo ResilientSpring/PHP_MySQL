c802131f42ec1db531f83d4f95d1324beca81fa6
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-11-03 14:19:10 +0800

    Committed 2021/11/03

--- Web Database Applications with PHP and MySQL/Chapter2 - The PHP Scripting Language/Concatenation.php
+++ Web Database Applications with PHP and MySQL/Chapter2 - The PHP Scripting Language/Concatenation.php
@@ -1,3 +1,26 @@
 <?php
 
+// Use the dot-operator (.) to concatenate two or more strings:
+$var = "test string";
 
+$var = "test"."string";
+
+$var = "test";
+
+$var = $var." string";
+
+// Here ia s shortcut to add a string to the end of another.
+$var .= " test";
+
+echo $var;
+
+print "<br/>";
+
+// The following are all equivalent.
+echo "test string";
+print "\n";
+echo "test" . "string";
+print "<br/>";
+echo "test"."string";
+print "<br>";
+echo "test", "string";
