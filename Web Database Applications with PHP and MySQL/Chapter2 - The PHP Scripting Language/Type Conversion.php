b907c7d4e2e8b9f6c883303311291f967b67ffe7
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-11-17 22:49:35 +0800

    Committed 2021/11/17

--- Web Database Applications with PHP and MySQL/Chapter2 - The PHP Scripting Language/Type Conversion.php
+++ Web Database Applications with PHP and MySQL/Chapter2 - The PHP Scripting Language/Type Conversion.php
@@ -1,1 +1,20 @@
 <?php
+
+$year = 2003;
+
+// Sets $yearString to the string value "2003"
+$yearString = strval($year);
+print $yearString;
+echo "<br>";
+
+$var = "abc";
+
+// Because the string "abc" doesn't look anything like an integer,
+// the call to intval() sets $value to zero.
+$value = intval($var);
+
+// sets $count to the integer value 2748
+$count = 2748;
+
+// Integer value of "abc" as a hexadecimal number
+$count = intval($var, 16);
