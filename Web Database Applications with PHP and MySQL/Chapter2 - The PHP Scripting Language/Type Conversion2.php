71af2144f3cf9a7dc4c966889f067af884f48536
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-11-18 00:57:19 +0800

    Committed 2021/11/17

---
+++ Web Database Applications with PHP and MySQL/Chapter2 - The PHP Scripting Language/Type Conversion2.php
@@ -0,0 +1,15 @@
+<?php
+
+$var = "abc";
+
+// Cast to float
+$float = (float) $var;
+print $float;
+echo "<br>";
+
+$float = floatval($var);
+print $float;
+echo "<br>";
+
+// cast to a string
+$string = (string) $var;
