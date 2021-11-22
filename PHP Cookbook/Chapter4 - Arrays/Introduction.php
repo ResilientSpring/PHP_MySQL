c4f347c51c4d5e6bb8f04a55ac5cd7e1700d5e82
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-11-22 18:48:17 +0800

    Committed 2021/11/22

---
+++ PHP Cookbook/Chapter4 - Arrays/Introduction.php
@@ -0,0 +1,12 @@
+<?php
+
+$fruits = array('Apples', 'Bananas', 'Cantaloupes', 'Dates');
+
+print $fruits[2];
+
+$fruits[0] = 'Apples';
+$fruits[1] = 'Kiwi';
+$fruits[2] = 'Lemon';
+$fruits[3] = 'Watermelon';
+
+print $fruits[2];
