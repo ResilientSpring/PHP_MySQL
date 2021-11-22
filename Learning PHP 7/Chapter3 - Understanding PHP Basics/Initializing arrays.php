ea002bda188fa78b7e9a90abc5bc472d1799fc4c
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-11-22 17:05:20 +0800

    Committed 2021/11/22

--- Learning PHP 7/Chapter3 - Understanding PHP Basics/Initializing arrays.php
+++ Learning PHP 7/Chapter3 - Understanding PHP Basics/Initializing arrays.php
@@ -1,1 +1,7 @@
 <?php
+$empty1 = [];
+$empty2 = array();
+$names1 = ['Harry', 'Ron', 'Hermione'];
+$names2 = array('Harry', 'Ron', 'Hermione');
+$status1 = ['name' => 'James Potter', 'status' => 'dead'];
+$status2 = array('name' => 'James Potter', 'status' => 'dead');
