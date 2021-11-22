5ff2903416e2a09382dded8ddc1549452b93ac43
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-11-22 18:15:33 +0800

    Committed 2021/11/22

---
+++ Supplementary materials/Array.php
@@ -0,0 +1,8 @@
+<?php
+
+$capital = array('Cambodia' => 'Phom Penh', 'Lebanon' => 'Beirut', 'Cuba' => 'Habana',
+    'Indonesia' => 'Jakarta', 'Finland' => 'Helsinki');
+
+print $capital['Cambodia'];
+
+# Example from book Learning PHP and MySQL
