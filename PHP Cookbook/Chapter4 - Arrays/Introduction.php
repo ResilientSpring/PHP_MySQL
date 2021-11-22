0c070a10598cba1e62c2134f32a1a48bb4469edf
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-11-22 19:12:56 +0800

    Committed 2021/11/22

--- PHP Cookbook/Chapter4 - Arrays/Introduction.php
+++ PHP Cookbook/Chapter4 - Arrays/Introduction.php
@@ -4,9 +4,21 @@ $fruits = array('Apples', 'Bananas', 'Cantaloupes', 'Dates');
 
 print $fruits[2];
 
+echo "<br>";
+
 $fruits[0] = 'Apples';
 $fruits[1] = 'Kiwi';
 $fruits[2] = 'Lemon';
 $fruits[3] = 'Watermelon';
 
 print $fruits[2];
+
+echo "<br>";
+
+$concede = [];
+
+$concede[] = 'Apple';
+
+print $concede[0];
+
+echo "<br>";
