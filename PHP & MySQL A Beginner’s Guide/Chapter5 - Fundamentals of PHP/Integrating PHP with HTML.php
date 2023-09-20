c70b52b1732f1236def66d59fac573032e4e88aa
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-09-20 22:59:58 +0800

    Committed on or around 2023/09/20

--- PHP & MySQL A Beginner’s Guide/Chapter5 - Fundamentals of PHP/Integrating PHP with HTML.php
+++ PHP & MySQL A Beginner’s Guide/Chapter5 - Fundamentals of PHP/Integrating PHP with HTML.php
@@ -9,9 +9,19 @@
     </title>
 </head>
 
+<body>
+
+<h1>This is an Example of PHP Embedded in HTML</h1>
+
+<p>The PHP code displays information about the PHP server.</p>
+
 <?php
 
+phpinfo();
+
 ?>
+
+</body>
 
 </html>
 
