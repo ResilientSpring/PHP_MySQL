64b4678041bb1d811350c1a67108c523a411b6dd
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-10-03 11:13:15 +0800

    Committed on or around 2023/10/02

---
+++ PHP - A Beginner's Guide/Chapter2 - Variables and Operators/Concatenating Strings.php
@@ -0,0 +1,10 @@
+<?php
+// define variables
+$country = 'England';
+$city = "London";
+// combine into single string
+// output: "Welcome to London, the coolest city in all of the England"
+echo "Welcome to ".$city.", the coolest city in all of ".$country;
+
+?>
+
