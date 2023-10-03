ce425527fc53525865e476dea9bd7bf70a8ff861
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-10-03 11:17:40 +0800

    Committed on or around 2023/10/03

---
+++ PHP - A Beginner's Guide/Chapter2 - Variables and Operators/Comparing Variables.php
@@ -0,0 +1,21 @@
+<?php
+// define variables
+$p = 10;
+$q = 11;
+$r = 11.3;
+$s = 11;
+
+// test if $q is greater than $p
+// return true
+echo ($q > $p);
+
+// test if $q is less than $p
+// return false
+echo ($q < $p);
+
+// test if $q is greater than or equal to $s
+// return true
+echo ($q >= $s);
+
+
+
