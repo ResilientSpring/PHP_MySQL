77bdeae48ca267877706a762b24632e3525724e1
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-10-03 12:42:23 +0800

    Committed on or around 2023/10/03

---
+++ PHP - A Beginner's Guide/Chapter2 - Variables and Operators/Try This 2-1 Building a Dollars-to-Euros Converter.php
@@ -0,0 +1,22 @@
+<html>
+
+<head>
+    <title>
+        Project 2-1: USD/EUR Currency Conversion
+    </title>
+</head>
+
+<body>
+
+<h2>Project 2-1 USD/EUR Currency Conversion</h2>
+
+<?php
+
+
+?>
+
+</body>
+
+</html>
+
+
