489fd3ede143ffefa81264cb19d770d66c3d9aed
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-10-03 12:18:21 +0800

    Committed on or around 2023/10/03

---
+++ PHP - A Beginner's Guide/Chapter2 - Variables and Operators/Other Useful Operators.php
@@ -0,0 +1,9 @@
+<?php
+// define variables
+$count = 7;
+$age = 60;
+$greeting = "We";
+
+// subtract 2 and re-assign new value to variable equivalent to $count = $count -2
+// output: 5
+$count -= 2;
