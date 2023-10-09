a2abdc940dae1e9e807b025ef47bd826ecaa088d
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-10-09 16:36:16 +0800

    Committed on or around 2023/10/09

--- PHP - A Beginner's Guide/Chapter2 - Variables and Operators/car.php
+++ PHP - A Beginner's Guide/Chapter2 - Variables and Operators/car.php
@@ -6,9 +6,21 @@
 
     <h2>Success!</h2>
 
+
+<?php
+
+// get from input
+$type = $_POST['selType'];
+$color = $_POST['txtColor'];
+
+// use form input
+echo "Your $color $type is ready. Safe driving!";
+
+?>
+
+
 </body>
 
 
 </html>
 
-<?php
