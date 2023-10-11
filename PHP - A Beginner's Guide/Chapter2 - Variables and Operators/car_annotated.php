75c2d19b3540cae682d209b6b9a0c104b648eae1
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-10-11 22:58:51 +0800

    Committed on or around 2023/10/11

--- PHP - A Beginner's Guide/Chapter2 - Variables and Operators/car_annotated.php
+++ PHP - A Beginner's Guide/Chapter2 - Variables and Operators/car_annotated.php
@@ -10,7 +10,11 @@
 
 /* Whenever a form is submitted to a PHP script through the POST method,
    the form's input variables and their values automatically become available
- * to
+ * to the PHP script through a special container variable named $_POST.
+ *
+ * Accessing the value entered into a particular form field then becomes as
+ * simple as referencing $_POST with the field's name.
+ *
  * */
 $type = $_POST["selType"];
 $color = $_POST["txtColor"];
