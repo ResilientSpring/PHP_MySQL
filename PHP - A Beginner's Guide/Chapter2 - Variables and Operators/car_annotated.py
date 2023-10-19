a6e8515253c606f915c41c0f98e434429192509a
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-10-19 22:15:19 +0800

    Committed on or around 2023/10/19

--- PHP - A Beginner's Guide/Chapter2 - Variables and Operators/car_annotated.py
+++ PHP - A Beginner's Guide/Chapter2 - Variables and Operators/car_annotated.py
@@ -1,4 +1,6 @@
 _POST = {"TypeSelected": "Porsche 911", "txtColor": "Green"}
 
 type = _POST["TypeSelected"]
+color = _POST["txtColor"]
 
+print("Your ", color, " ", type, " is ready. Safe driving!")
