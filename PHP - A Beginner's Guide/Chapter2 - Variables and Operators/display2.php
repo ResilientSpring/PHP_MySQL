b33f95e9760e1bdc68da608b3feb698401c6a0d6
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-10-20 00:01:22 +0800

    Committed on or around 2023/10/20

---
+++ PHP - A Beginner's Guide/Chapter2 - Variables and Operators/display2.php
@@ -0,0 +1,35 @@
+<html>
+
+<head>
+    <title>Project 2-2: An Interactive HTML Color Sampler</title>
+</head>
+
+<body>
+
+<h2>Project 2-2: An Interactive HTML Color Sampler</h2>
+
+<?php
+
+// get input values
+
+$r = $HTTP_GET_VARS['r'];
+$g = $HTTP_GET_VARS['g'];
+$b = $HTTP_GET_VARS['b'];
+
+// generate RGB string from input
+$rgb = $r.','.$g.','.$b;
+?>
+
+R:<?php echo $r; ?>
+G:<?php echo $g; ?>
+B:<?php echo $b; ?>
+
+<p/>
+
+<div style="width:150px; height:150px; background-color: rgb(<?php echo $rgb; ?>)"
+
+</body>
+
+</html>
+
+
