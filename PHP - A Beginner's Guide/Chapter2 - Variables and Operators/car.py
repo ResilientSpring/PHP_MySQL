2a5ffab54f8ce15e0da96b10c94dedd63d625c8b
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-10-19 22:25:14 +0800

    Committed on or around 2023/10/19

--- PHP - A Beginner's Guide/Chapter2 - Variables and Operators/car.py
+++ PHP - A Beginner's Guide/Chapter2 - Variables and Operators/car.py
@@ -1,1 +1,6 @@
-_POST = {}
+_POST = {'selType': 'Volkswagen Beetle', 'txtColor': 'pink'}
+
+type = _POST['selType']
+color = _POST['txtColor']
+
+print("Your", color, type, "is ready. Safe driving!")
