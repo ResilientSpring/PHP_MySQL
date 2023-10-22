958c5c371addebbe67cfa53d26eaa09365487c75
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-10-23 00:08:53 +0800

    Committed on or around 2023/10/21

---
+++ PHP & MySQL A Beginner’s Guide/Chapter5 - Fundamentals of PHP/PHP Basics.php
@@ -0,0 +1,26 @@
+<html>
+
+<head>
+    <title>Listing 5-2</title>
+</head>
+
+<body>
+
+<h1>
+    This shows the print and echo functions
+</h1>
+
+<?php
+
+print "This was produced by print <br>";
+echo "This was produced by echo ";
+
+?>
+
+
+</body>
+
+
+</html>
+
+
