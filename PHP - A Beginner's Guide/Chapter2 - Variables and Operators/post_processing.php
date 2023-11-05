3da2ef61c53bc907eff798290bc12435aae72e7e
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-11-05 22:12:35 +0800

    Committed on or around 2023/11/05

--- PHP - A Beginner's Guide/Chapter2 - Variables and Operators/post_processing.php
+++ PHP - A Beginner's Guide/Chapter2 - Variables and Operators/post_processing.php
@@ -1,6 +1,19 @@
+<html>
+
+<title>Converted Result</title>
+
+
+<h4>Here is the result of conversion</h4>
+
 <?php
 
 $original_money = $_POST['original_money'];
 $exchange_rate = $_POST['exchange_rate'];
 $amount_to_be_converted = $_POST['amount'];
 $targeted_money = $_POST['target_money'];
+
+?>
+
+
+
+</html>
