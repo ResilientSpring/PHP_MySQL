a374cafa4c7be4199eb1d4e58e102f486956f4ff
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-11-05 22:22:27 +0800

    Committed on or around 2023/11/05

--- PHP - A Beginner's Guide/Chapter2 - Variables and Operators/post_processing.php
+++ PHP - A Beginner's Guide/Chapter2 - Variables and Operators/post_processing.php
@@ -16,7 +16,7 @@ $targeted_money = $_POST['target_money'];
 
 <p>
 
-    <?php echo $amount_to_be_converted." of ".$original_money ?> equal
+    <?php echo $amount_to_be_converted." of ".$original_money."s" ?> equal
     <?php echo $amount_to_be_converted * $exchange_rate?> of
     <?php echo " ".$targeted_money?>s.
 
