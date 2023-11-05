712fc9018a9df22682a76377127fdee952a6ef58
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-11-05 17:03:26 +0800

    Committed on or around 2023/11/05

---
+++ PHP - A Beginner's Guide/Chapter2 - Variables and Operators/Setting and Checking Variable Data Types 4.php
@@ -0,0 +1,19 @@
+<?php
+
+$lineage = true;
+$apt = 58.9;
+$interpolate = 7;
+
+echo is_bool($lineage);
+
+echo "<br>";
+
+echo is_int($apt);
+
+print "/n";
+
+echo is_numeric($interpolate);
+
+print "<br/>";
+
+print is_numeric($apt);
