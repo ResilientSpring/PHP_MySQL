22ce6f3bf875121ed23a5fb9df00956330653da6
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-11-14 11:03:28 +0800

    Committed on or around 2023/11/14

--- PHP - A Beginner's Guide/Chapter2 - Variables and Operators/Self-Test 10.php
+++ PHP - A Beginner's Guide/Chapter2 - Variables and Operators/Self-Test 10.php
@@ -1,5 +1,18 @@
+<html>
+
+
 <?php
 $celsius = $_GET["celsius"];
 
 $fahrenheit = (9/5) * $celsius + 32;
 
+
+?>
+
+<script>
+
+    document.write(<?php print $fahrenheit?>"<span style="text-transform:uppercase">o</span>F");
+
+</script>
+
+</html>
