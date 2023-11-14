a1e0922ee999dc68cd2465adb861b37ad84acee7
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-11-14 20:00:18 +0800

    Committed on or around 2023/11/14

--- Programming in HTML and PHP/Chapter6 - Arrays/Cloud_observation.php
+++ Programming in HTML and PHP/Chapter6 - Arrays/Cloud_observation.php
@@ -1,4 +1,5 @@
 <?php
+
 // high clouds
 if (isset($_POST ["high"])) {
     $high = $_POST ["high"];
@@ -10,37 +11,39 @@ if (isset($_POST ["high"])) {
 } else {
     echo "You didn't observe any high clouds.<br />";
 }
+
 // mid clouds
 if (isset($_POST ["mid"])) {
     $mid = $_POST ["mid"];
     $n = count($mid);
-    echo "For mid clouds, you observed<br />";
+    echo "<br>For mid-clouds, you observed<br />";
     for ($i = 0; $i < $n; $i++) {
         echo $mid [$i] . "<br />";
     }
 } else {
-    echo "You didn't observe any mid clouds.<br />";
+    echo "<br>You didn't observe any mid clouds.<br />";
 }
+
 // low clouds
 if (isset($_POST ["low"])) {
     $low = $_POST ["low"];
     $n = count($low);
-    echo "For low clouds, you observed<br />";
+    echo "<br>For low clouds, you observed<br />";
     for ($i = 0; $i < $n; $i++) {
         echo $low [$i] . "<br />";
     }
 } else {
-    echo "You didn't observe any low clouds.<br />";
+    echo "<br>You didn't observe any low clouds.<br />";
 }
 // rain clouds
 if (isset($_POST ["rain"])) {
     $rain = $_POST ["rain"];
     $n = count($rain);
-    echo "For rain clouds, you observed<br />";
+    echo "<br>For rain clouds, you observed<br />";
     for ($i = 0; $i < $n; $i++) {
         echo $rain [$i] . "<br />";
     }
 } else {
-    echo "You didn't observe any rain clouds.<br />";
+    echo "<br>You didn't observe any rain clouds.<br />";
 }
 ?>
