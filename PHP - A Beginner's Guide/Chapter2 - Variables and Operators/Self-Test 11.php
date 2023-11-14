6a0ee585e4250aaa9b43b06f79254615eef30f15
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-11-14 15:58:15 +0800

    Committed on or around 2023/11/14

--- PHP - A Beginner's Guide/Chapter2 - Variables and Operators/Self-Test 11.php
+++ PHP - A Beginner's Guide/Chapter2 - Variables and Operators/Self-Test 11.php
@@ -5,7 +5,7 @@ $password_field = $_POST["password_field"];
 $email = $_POST["text_area"];
 $selection_list = $_POST["selection_list"];
 
-echo "Your selection is ".$selection_list.".<br>";
+echo "Hi, $text_input_field! Your selection is ".$selection_list.".<br>";
 
 $radio_buttons = $_POST["radio_buttons"];
 
