e406998cdcb1b78b2de364e5cecc290377540f0d
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-11-15 11:28:32 +0800

    Committed on or around 2023/11/15

---
+++ PHP - A Beginner's Guide/Chapter3 - Controlling Program Flow/Try This 3-1 Testing Odd and Even Numbers 2.php
@@ -0,0 +1,52 @@
+<html>
+
+<head>
+    <title>Project 3-1: Odd/Even Number Tester</title>
+</head>
+
+<body>
+
+<h2>Project 3-1: Odd/Even Number Tester</h2>
+
+<?php
+
+// if form not yet submitted, display form
+if (!isset($_POST['submit'])) {
+
+    ?>
+
+    <form method="post" action="Try%20This%203-1%20Testing%20Odd%20and%20Even%20Numbers.php">
+
+        Enter value: <br>
+
+        <input type="text" name="num" size="3" id="hi" />
+        <label for="hi"></label>
+
+        <p></p>
+
+        <input type="submit" name="submit" value="Submit" />
+
+    </form>
+
+    <?php
+
+// if form submitted, process form input
+
+} else {
+
+    // retrieve number from POST submission
+    $num = $_POST['num'];
+
+    // test value for even-ness
+    // display appropriate message
+    if (($num % 2) == 0)
+        echo 'You entered '.$num.', which is an even number.';
+    else
+        echo 'You entered'.$num.', which is an odd number';
+
+}
+?>
+
+
+</body>
+</html>
