8c944a5499abffc78b73db6305ed77245762aea0
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-11-15 10:52:58 +0800

    Committed on or around 2023/11/15

--- PHP - A Beginner's Guide/Chapter3 - Controlling Program Flow/Try This 3-1 Testing Odd and Even Numbers.php
+++ PHP - A Beginner's Guide/Chapter3 - Controlling Program Flow/Try This 3-1 Testing Odd and Even Numbers.php
@@ -8,6 +8,21 @@
 
     <h2>Project 3-1: Odd/Even Number Tester</h2>
 
+<?php
+
+// if form not yet submitted, display form
+if (isset($_POST['submit'])) {
+
+?>
+
+<form method="post" action="Try%20This%203-1%20Testing%20Odd%20and%20Even%20Numbers.php">
+
+    Enter value: <br>
+
+    <input type="text" name="num" size="3" />
+
+</form>
+
 
 </body>
 
