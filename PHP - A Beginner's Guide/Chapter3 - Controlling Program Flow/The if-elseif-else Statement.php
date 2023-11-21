20ad88e2ddfc58232fe7896fb2921acf3405e9e1
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-11-21 23:58:00 +0800

    Committed on or around 2023/11/21

--- PHP - A Beginner's Guide/Chapter3 - Controlling Program Flow/The if-elseif-else Statement.php
+++ PHP - A Beginner's Guide/Chapter3 - Controlling Program Flow/The if-elseif-else Statement.php
@@ -9,4 +9,8 @@ if ($today = 'Monday')
     echo 'Monday\'s child is fair of face.';
 elseif ($today == 'Tuesday')
     echo 'Tuesday\'s child is full of grace.';
+elseif ($today == 'Wednesday')
+    echo 'Wednesday\'s child is full of woe.';
+elseif ($today == 'Thursday')
+    echo 'Thursday\'s child has far to go.';
 
