b7e63d54dea5012c33ad96edd7ec12b9dcc24d61
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-11-23 23:42:48 +0800

    Committed on or around 2023/11/23

--- PHP - A Beginner's Guide/Chapter3 - Controlling Program Flow/Try This 3-2 Assigning Boy Scouts to Tents.php
+++ PHP - A Beginner's Guide/Chapter3 - Controlling Program Flow/Try This 3-2 Assigning Boy Scouts to Tents.php
@@ -29,6 +29,21 @@ if (!isset($_POST["submit"])) {
 
 <?php
 
+} else {
+
+    $age = $_POST["age"];
+
+    if ($age <= 9)
+        echo "You are in the Red tent.";
+    elseif ($age > 9 && $age <= 11)
+        echo "You are in the Blue tent.";
+    elseif ($age > 11 && $age <= 14)
+        echo "You are in the Green tent.";
+    elseif ($age > 14 && $age <= 17)
+        echo "You are in the Black tent.";
+    else
+        echo "You'd better get in touch with the Scoutmaster.";
+
 }
 
 
