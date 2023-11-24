ad17b8da714c330ba71846bddb280d648820e974
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-11-24 12:05:03 +0800

    Committed on or around 2023/11/24

--- PHP - A Beginner's Guide/Chapter3 - Controlling Program Flow/Combining Conditional Statements.php
+++ PHP - A Beginner's Guide/Chapter3 - Controlling Program Flow/Combining Conditional Statements.php
@@ -12,32 +12,29 @@
 
 <?php
 
-if (!isset($_POST["submit"])){
+if (!isset($_POST["submit"])) {
 
-?>
+    ?>
 
-<form method="post" action="Combining%20Conditional%20Statements.php">
+    <form method="post" action="Combining%20Conditional%20Statements.php">
 
-    <label for="rating">Rating: </label><br>
-    <input id="rating" type="text" name="$rating" />
+        <label for="rating">Rating: </label><br>
+        <input id="rating" type="text" name="$rating"/>
 
-    <label for="current_salary">Current salary: </label><br>
-    <input id="current_salary" type="text" name="current_salary" />
+        <label for="current_salary">Current salary: </label><br>
+        <input id="current_salary" type="text" name="current_salary"/>
 
-    <input type="submit" name="submit" value="Submit" />
+        <input type="submit" name="submit" value="Submit"/>
 
-</form>
+    </form>
 
 
-<?php
+    <?php
 
-} else{
-
+} else {
 
 
 }
-
-
 
 
 ?>
