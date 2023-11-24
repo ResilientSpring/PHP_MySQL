f0e7aad18e693ac5eb09268e846f44c7c0aedddd
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-11-24 13:17:25 +0800

    Committed on or around 2023/11/24

---
+++ PHP - A Beginner's Guide/Chapter3 - Controlling Program Flow/Combining_Conditional_Statements_4.php
@@ -0,0 +1,46 @@
+<html>
+
+<header>
+    <title>Combining Conditional Statements</title>
+</header>
+
+<body>
+
+<?php
+
+if (!isset($_POST["submit"])) {
+
+    ?>
+
+    <h3>Let's see if your entered year is a leap year.</h3>
+
+    <form method="post" action="Combining_Conditional_Statements_4.php">
+
+        <label for="year">Year: </label><br>
+        <input type="text" name="year" id="year" size="15"/>
+
+        <input type="submit" name="submit" value="Submit!"/>
+        <input type="reset" name="reset" value="Reset" />
+
+    </form>
+
+    <?php
+
+} else{
+
+    $year = $_POST["year"];
+
+    if (($year % 400 == 0) || (($year % 100 != 0) && ($year % 4 ==0)))
+        echo "$year is a leap year.";
+    else
+        echo "$year is not a leap year.";
+
+}
+
+?>
+
+</body>
+
+
+</html>
+
