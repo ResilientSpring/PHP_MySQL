6cb4fc238c2ed8e2203fb67745916a4aaa05d5d4
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-11-24 11:59:53 +0800

    Committed on or around 2023/11/24

--- PHP - A Beginner's Guide/Chapter3 - Controlling Program Flow/Combining Conditional Statements.php
+++ PHP - A Beginner's Guide/Chapter3 - Controlling Program Flow/Combining Conditional Statements.php
@@ -12,6 +12,14 @@ if (!isset($_POST["submit"])){
 
 ?>
 
+<form method="post" action="Combining%20Conditional%20Statements.php">
+
+    <label for="rating">Rating: </label><br>
+    <input id="rating" type="text" name="$rating" />
+
+
+
+</form>
 
 
 <?php
