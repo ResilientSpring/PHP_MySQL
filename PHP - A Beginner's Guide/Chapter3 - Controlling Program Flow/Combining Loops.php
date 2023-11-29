1c4e076cd0c0d26aae604d9a46951b687e9c1672
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-11-29 23:53:51 +0800

    Committed on or around 2023/11/29

--- PHP - A Beginner's Guide/Chapter3 - Controlling Program Flow/Combining Loops.php
+++ PHP - A Beginner's Guide/Chapter3 - Controlling Program Flow/Combining Loops.php
@@ -7,10 +7,24 @@
 
 <body>
 
+<?php
 
+// generate an HTML table
+// 3 rows, 4 columns
 
+echo "<table border=\"1\" >";
+
+for ($row = 1; $row < 4; $row++) {
+    echo "<tr>";
+
+    for ($col = 1; $col < 5; $col++)
+        echo "<td>Row $row, Column $col </td>";
+
+    echo "</table>";
+
+}
+
+?>
 
 </body>
-
-
 </html>
