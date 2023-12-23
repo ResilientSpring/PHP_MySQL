6729bdadf3f9c10d37fc4ff0a7cf5069fdf4c9e5
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-12-23 22:40:47 +0800

    Committed on or around 2023/12/23

---
+++ PHP - A Beginner's Guide/Chapter3 - Controlling Program Flow/Try_This_3-3 exit().php
@@ -0,0 +1,48 @@
+<html>
+
+<head>
+    <title>Project 3-3: Factorial Calculator</title>
+</head>
+
+<body>
+
+<h2>Project 3-3:Factorial Calculator</h2>
+
+<?php
+
+if(!isset($_POST['submit'])){
+
+    ?>
+
+    <form method="post" action="Try_This_3-3.php">
+        Enter a number: <br>
+        <input type="text" name="num" size="3" />
+        <p/>
+        <input type="submit" name="submit" value="Submit" />
+    </form>
+
+    <?php
+
+} else{
+
+    $num = $_POST['num'];
+
+    if ($num <= 0){
+        echo 'ERROR: Please enter a number greater than 0';
+        exit();
+    }
+
+    $factorial = 1;
+
+    for ($x = $num; $x >=1; $x--)
+        $factorial  *= $x;
+
+    echo "Factorial of $num is: $factorial";
+
+}
+
+?>
+
+</body>
+
+</html>
