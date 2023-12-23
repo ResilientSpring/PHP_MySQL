2b304c4f0db7e7e0a06a5344892c100fd9d122ae
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-12-23 22:23:47 +0800

    Committed on or around 2023/12/23

--- PHP - A Beginner's Guide/Chapter3 - Controlling Program Flow/Try_This_3-3.php
+++ PHP - A Beginner's Guide/Chapter3 - Controlling Program Flow/Try_This_3-3.php
@@ -8,6 +8,35 @@
 
 <h2>Project 3-3:Factorial Calculator</h2>
 
+<?php
+
+if(!isset($_POST['submit'])){
+
+?>
+
+    <form method="post" action="Try_This_3-3.php">
+        Enter a number: <br>
+        <input type="text" name="num" size="3" />
+        <p/>
+        <input type="submit" name="submit" value="Submit" />
+    </form>
+
+<?php
+
+} else{
+
+    $num = $_POST['num'];
+
+    if ($num <= 0){
+        echo 'ERROR: Please enter a number greater than 0';
+        exit();
+    }
+
+}
+
+
+?>
+
 </body>
 
 </html>
