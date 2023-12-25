4c603fd7c04b3e19af46d98ecde0b974f4118033
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-12-25 12:30:50 +0800

    Committed on or around 2023/12/25

--- PHP - A Beginner's Guide/Chapter3 - Controlling Program Flow/Working with HTML Strings.php
+++ PHP - A Beginner's Guide/Chapter3 - Controlling Program Flow/Working with HTML Strings.php
@@ -7,3 +7,8 @@ echo htmlentities($html);
 $html2 = '<div width="200">This is a div</div>';
 
 echo "<br><br>".htmlentities($html2);
+
+$lines = "This is a bro
+ken line";
+
+echo "<br><br>".nl2br($lines);
