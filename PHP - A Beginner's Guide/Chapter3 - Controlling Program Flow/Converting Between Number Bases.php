6fb4e8e221255e4bda9ce050561d6ad278ebc8d5
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-12-25 13:13:24 +0800

    Committed on or around 2023/12/25

--- PHP - A Beginner's Guide/Chapter3 - Controlling Program Flow/Converting Between Number Bases.php
+++ PHP - A Beginner's Guide/Chapter3 - Controlling Program Flow/Converting Between Number Bases.php
@@ -3,7 +3,17 @@
 // Convert from decimal to binary.
 echo decbin(8)."<br>";
 
+// Convert from decimal to hexadecimal.
+echo dechex(8)."<br>";
 
+// Convert from decimal to octal.
+echo decoct(8)."<br>";
 
+// Convert from octal to decimal.
+echo octdec(10)."<br>";
 
+// Convert from hexadecimal to decimal.
+echo hexdec(65)."<br>";
 
+// Convert from binary to decimal.
+echo bindec(1000);
