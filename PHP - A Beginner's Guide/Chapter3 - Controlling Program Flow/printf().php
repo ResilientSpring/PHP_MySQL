6145f90e3431b78705bc0234990545240d663cc9
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-12-26 23:39:50 +0800

    Committed on or around 2023/12/26

--- PHP - A Beginner's Guide/Chapter3 - Controlling Program Flow/printf().php
+++ PHP - A Beginner's Guide/Chapter3 - Controlling Program Flow/printf().php
@@ -1,9 +1,11 @@
 <?php
 
-printf("%05d", 65)."<br>";
+// printf("%05d", 65)."<br>";    printf() and print() are not the same.
+
+printf("%05d", 65);
 
 printf("%09.3f", 239)."<br>";
 
-printf("%4o", 8);
+printf("%4o", 8)."<br>";
 
 printf("I see %4d apples and %4.2f oranges", 8, 26);
