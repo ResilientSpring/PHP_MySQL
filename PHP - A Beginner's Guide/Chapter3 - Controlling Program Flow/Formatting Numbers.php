6826d1460c91b0dd0f1db2a470b789f91a50e10f
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-12-26 23:32:52 +0800

    Committed on or around 2023/12/26

--- PHP - A Beginner's Guide/Chapter3 - Controlling Program Flow/Formatting Numbers.php
+++ PHP - A Beginner's Guide/Chapter3 - Controlling Program Flow/Formatting Numbers.php
@@ -3,7 +3,7 @@
 $num = 1106482.5843;
 
 // format number with defaults
-echo number_format($num);
+echo number_format($num)."<br>";
 
 // format number with custom separators
 echo number_format($num, 3, '*', '?');
