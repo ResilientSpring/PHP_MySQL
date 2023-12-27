e5dd23f8b39f6f4e2582a43a590c8cd802b62bcd
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-12-28 00:33:52 +0800

    Committed on or around 2023/12/28

--- PHP - A Beginner's Guide/Chapter3 - Controlling Program Flow/register.php
+++ PHP - A Beginner's Guide/Chapter3 - Controlling Program Flow/register.php
@@ -1,1 +1,22 @@
+<html>
+
+<head>
+    <title>Project 3-4: Member Registration</title>
+</head>
+
+<body>
+
+<h2>Project 3-4: Member Registration</h2>
+
 <?php
+
+$name = $_POST["name"];
+$address = $_POST["address"];
+
+
+?>
+
+
+</body>
+
+</html>
