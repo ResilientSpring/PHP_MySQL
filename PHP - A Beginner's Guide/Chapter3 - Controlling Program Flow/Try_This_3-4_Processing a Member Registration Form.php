1665c072ab95d2b7ae8272fc7d6cbd42f18560fe
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2023-12-28 23:29:13 +0800

    Committed on or around 2023/12/28

--- PHP - A Beginner's Guide/Chapter3 - Controlling Program Flow/Try_This_3-4_Processing a Member Registration Form.php
+++ PHP - A Beginner's Guide/Chapter3 - Controlling Program Flow/Try_This_3-4_Processing a Member Registration Form.php
@@ -17,34 +17,41 @@ $profession = $_POST["profession"];
 $resident = $_POST["resident"];
 
 
-if (empty($name))
+if (empty($name)) {
     die("ERROR: Please provide your name.");
+}
 
 
-if (empty($address))
+if (empty($address)) {
     die("ERROR: Please provide your address.");
+}
 
-if (empty($age))
+if (empty($age)) {
     die("ERROR: Please provide your age.");
-elseif ($age < 18 || $age > 60 )
+} elseif ($age < 18 || $age > 60) {
     die("ERROR: Membership is only open to those between 18 and 60 years.");
+}
 
-if (empty($profession))
+if (empty($profession)) {
     die("ERROR: Please provide your profession.");
+}
 
-if (strcmp($resident, 'no') == 0)
+if (strcmp($resident, 'no') == 0) {
     die("ERROR: Membership is only open to residents.");
+}
 
 
 $to = 'registration@some.domain.com';
 $from = 'webmaster@some.domain.com';
 $subject = 'Application for membership';
-$body = "Name: $name\r\nAddress: $address\r\nAge: $age\r\nProfession: $profession\r\n";
+$body
+    = "Name: $name\r\nAddress: $address\r\nAge: $age\r\nProfession: $profession\r\n";
 
-if (mail($to, $subject, $body, "From: $from"))
+if (mail($to, $subject, $body, "From: $from")) {
     echo "Thank you for your application.";
-else
+} else {
     die("ERROR: Mail delivery error.");
+}
 
 ?>
 
