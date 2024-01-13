5af86fec979b30853f0537ac044b8685061c835f
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-01-13 23:57:01 +0800

    Committed on or around 2024/1/13

--- PHP - A Beginner's Guide/Chapter3 - Controlling Program Flow/Self-Test No.5.php
+++ PHP - A Beginner's Guide/Chapter3 - Controlling Program Flow/Self-Test No.5.php
@@ -1,6 +1,5 @@
 <?php
 
-for ($multiplier = 1; $multiplier <= 10; $multiplier ++){
-
+for ($multiplier = 1; $multiplier <= 10; $multiplier++)
     echo "8 x $multiplier = ". 8 * $multiplier."<br>";
-}
+
