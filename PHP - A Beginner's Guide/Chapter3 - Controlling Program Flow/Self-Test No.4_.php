2d2ab254fdba2f1da78d520a492e5f2a04df33b7
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-01-13 23:52:09 +0800

    Committed on or around 2024/1/13

--- PHP - A Beginner's Guide/Chapter3 - Controlling Program Flow/Self-Test No.4_.php
+++ PHP - A Beginner's Guide/Chapter3 - Controlling Program Flow/Self-Test No.4_.php
@@ -4,9 +4,7 @@ $multiplier = 1;
 
 while ($multiplier <= 10){
 
-    echo "8 x $multiplier = ";
-    echo 8 * $multiplier;
-    echo "<br>";
+    echo "8 x $multiplier = ". 8 * $multiplier."<br>";
 
     $multiplier++;
 
