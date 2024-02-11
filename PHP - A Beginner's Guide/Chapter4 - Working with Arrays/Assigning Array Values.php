ae75a72102b23d0ac77354aaf72bfc3c5b51c5d2
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-02-11 23:48:58 +0800

    Committed on or around 2024/02/11

---
+++ PHP - A Beginner's Guide/Chapter4 - Working with Arrays/Assigning Array Values.php
@@ -0,0 +1,10 @@
+<?php
+
+// define array
+$cars = arrary('Ferrari', 'Porsche', 'Jaguar', 'Lamborghini', 'Mercedes');
+
+$cars2[0] = 'Ferrari';
+$cars2[1] = 'Porsche';
+$cars2[2] = 'Jaguar';
+$cars2[3] = 'Lamborghini';
+$cars2[4] = 'Mercedes';
