8eba08d551872cd811079bbd0e9ad2e2129ec5da
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-02-11 23:52:10 +0800

    Committed on or around 2024/02/11

--- PHP - A Beginner's Guide/Chapter4 - Working with Arrays/Assigning Array Values.php
+++ PHP - A Beginner's Guide/Chapter4 - Working with Arrays/Assigning Array Values.php
@@ -8,3 +8,8 @@ $cars2[1] = 'Porsche';
 $cars2[2] = 'Jaguar';
 $cars2[3] = 'Lamborghini';
 $cars2[4] = 'Mercedes';
+
+$cars3[] = 'Ferrari';
+$cars3[] = 'Lamborghini';
+
+$data = array('username' => 'John', 'password' => 'secret', 'host' => '192.168.0.1');
