ce9dcf70e6f43c29c2bd4458c5c9a4569d2f78c4
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-02-25 23:51:47 +0800

    Committed on or around 2024/02/25

--- PHP - A Beginner's Guide/Chapter4 - Working with Arrays/Retrieving Array Size.php
+++ PHP - A Beginner's Guide/Chapter4 - Working with Arrays/Retrieving Array Size.php
@@ -3,4 +3,7 @@
 // define array
 // $data = {"apple"};
 
-$data = ["Aspire", "local host"];
+$data = ["Aspire", "local host", "orders", "database", "missing"];
+
+for ($i = 0; $i < sizeof($data); $i++)
+    echo $data[$i]."<br>";
