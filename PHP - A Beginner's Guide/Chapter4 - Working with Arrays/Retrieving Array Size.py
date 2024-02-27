30518d69f91e3e646ad76cee8d83b818ded704b0
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-02-27 21:59:56 +0800

    Committed on or around 2024/02/27

--- PHP - A Beginner's Guide/Chapter4 - Working with Arrays/Retrieving Array Size.py
+++ PHP - A Beginner's Guide/Chapter4 - Working with Arrays/Retrieving Array Size.py
@@ -2,4 +2,4 @@ states = {"Ohio": {"population": 11353140, "capital": "Columbus"},
           "Nebraska": {"population": 1711263, "capital": "Omaha"}
           }
 
-
+print(states["Ohio"]["population"])
