9a3a964d35a47780e412d46c1432bcc26a6787a0
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-03-13 00:28:19 +0800

    Committed on or around 2024/03/13

---
+++ PHP - A Beginner's Guide/Chapter4 - Working with Arrays/Nesting Arrays 2.php
@@ -0,0 +1,7 @@
+<?php
+
+$phone_book = [
+    ["name" => "Raymond Rabbit", "tel" => "1234567", "email" => "ray@bunnyplanet.in"],
+    ["tel" => "1234567"],
+    ["email" => "ray@bunnyplanet.in"],
+];
