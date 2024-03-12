13099aa8e80166a3e7e30b3d923b7235ce0a6fed
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-03-12 23:58:41 +0800

    Committed on or around 2024/03/12

---
+++ PHP - A Beginner's Guide/Chapter4 - Working with Arrays/Nesting Arrays.php
@@ -0,0 +1,15 @@
+<?php
+// define nested array
+
+$phonebook = array(
+    array(
+        'name' => 'Raymond Rabbit',
+        'tel' => '1234567',
+        'email' => 'ray@bunnyplanet.in',
+    ),
+    array(
+        'name' => 'David Duck',
+        'tel' => '8562940',
+        'email' => 'dduck@duckpond.corp',
+    ),
+)
