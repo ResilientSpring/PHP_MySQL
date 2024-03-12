45b02a0a4b4986e453176ae26173d902d9b00c1e
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-03-13 00:32:13 +0800

    Committed on or around 2024/03/13

--- PHP - A Beginner's Guide/Chapter4 - Working with Arrays/Nesting Arrays 2.php
+++ PHP - A Beginner's Guide/Chapter4 - Working with Arrays/Nesting Arrays 2.php
@@ -5,3 +5,5 @@ $phone_book = [
     ["name" => "David Duck", "tel" => "8562940", "email"=>"dduck@duckpond.corp"],
     ["name" => "Harold Horse", "tel" => "5942033", "email" => "kingharold@farmersmarket.horsestuff.com"],
 ];
+
+echo "David Duck's number is ".$phone_book[1]["tel"];
