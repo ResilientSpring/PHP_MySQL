b61098aa132920124b239f3ed80f58f6c986c6fe
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-04-16 13:38:43 +0800

    Committed on or around 2024/04/16

--- PHP 8 Solutions/Ch 12/images.sql
+++ PHP 8 Solutions/Ch 12/images.sql
@@ -11,9 +11,9 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT = @@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS = @@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION = @@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
 --
@@ -27,31 +27,34 @@ START TRANSACTION;
 --
 
 DROP TABLE IF EXISTS `images`;
-CREATE TABLE `images` (
-                          `image_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-                          `filename` varchar(25) NOT NULL,
-                          `caption` varchar(120) NOT NULL,
-                          PRIMARY KEY (`image_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+CREATE TABLE `images`
+(
+    `image_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+    `filename` varchar(25)      NOT NULL,
+    `caption`  varchar(120)     NOT NULL,
+    PRIMARY KEY (`image_id`)
+) ENGINE = InnoDB
+  AUTO_INCREMENT = 9
+  DEFAULT CHARSET = latin1;
 
 --
 -- Dumping data for table `images`
 --
 
-INSERT INTO `images` (`image_id`, `filename`, `caption`) VALUES
-                                                             (1, 'basin.jpg', 'Water basin at Ryoanji temple, Kyoto'),
-                                                             (2, 'fountains.jpg', 'Fountains in central Tokyo'),
-                                                             (3, 'kinkakuji.jpg', 'The Golden Pavilion in Kyoto'),
-                                                             (4, 'maiko.jpg', 'Maiko&mdash;trainee geishas in Kyoto'),
-                                                             (5, 'maiko_phone.jpg', 'Every maiko should have one&mdash;a mobile, of course'),
-                                                             (6, 'menu.jpg', 'Menu outside restaurant in Pontocho, Kyoto'),
-                                                             (7, 'monk.jpg', 'Monk begging for alms in Kyoto'),
-                                                             (8, 'ryoanji.jpg', 'Autumn leaves at Ryoanji temple, Kyoto');
+INSERT INTO `images` (`image_id`, `filename`, `caption`)
+VALUES (1, 'basin.jpg', 'Water basin at Ryoanji temple, Kyoto'),
+       (2, 'fountains.jpg', 'Fountains in central Tokyo'),
+       (3, 'kinkakuji.jpg', 'The Golden Pavilion in Kyoto'),
+       (4, 'maiko.jpg', 'Maiko&mdash;trainee geishas in Kyoto'),
+       (5, 'maiko_phone.jpg', 'Every maiko should have one&mdash;a mobile, of course'),
+       (6, 'menu.jpg', 'Menu outside restaurant in Pontocho, Kyoto'),
+       (7, 'monk.jpg', 'Monk begging for alms in Kyoto'),
+       (8, 'ryoanji.jpg', 'Autumn leaves at Ryoanji temple, Kyoto');
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40101 SET CHARACTER_SET_CLIENT = @OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS = @OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION = @OLD_COLLATION_CONNECTION */;
 
 /* Sources and References:
 
