bcc58feff81a2bc8ab608a46383f4adb429428df
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-04-17 11:44:30 +0800

    Committed on or around 2024/04/17

--- PHP - A Beginner's Guide/Chapter7 - Working with Database and MySQL/Adding Tables 2.sql
+++ PHP - A Beginner's Guide/Chapter7 - Working with Database and MySQL/Adding Tables 2.sql
@@ -1,2 +1,9 @@
 DROP TABLE IF EXISTS artists;
 
+CREATE TABLE artists(
+
+    artist_id INT(4) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    artist_name VARCHAR(50) NOT NULL ,
+    artist_country VARCHAR(2) NOT NULL
+
+)
