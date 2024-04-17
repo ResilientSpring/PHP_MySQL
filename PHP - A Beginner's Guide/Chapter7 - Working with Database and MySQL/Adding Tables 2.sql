ef94a6f745747ab226cc57cb2a41380c9fbc98b2
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-04-17 11:50:08 +0800

    Committed on or around 2024/04/17

--- PHP - A Beginner's Guide/Chapter7 - Working with Database and MySQL/Adding Tables 2.sql
+++ PHP - A Beginner's Guide/Chapter7 - Working with Database and MySQL/Adding Tables 2.sql
@@ -6,4 +6,17 @@ CREATE TABLE artists(
     artist_name VARCHAR(50) NOT NULL ,
     artist_country VARCHAR(2) NOT NULL
 
-)
+);
+
+CREATE TABLE ratings(
+    rating_id INT(2) NOT NULL PRIMARY KEY,
+    rating_name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE songs(
+
+    song_id INT(4) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    song_title VARCHAR(100) NOT NULL ,
+    foreign_key_song_artist INT(4)  ,
+    foreign_key_song_rating INT(2)
+);
