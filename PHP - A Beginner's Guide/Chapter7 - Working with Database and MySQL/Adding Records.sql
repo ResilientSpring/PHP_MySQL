7f0485961c53107292fc2bf102c33caec0c506a0
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-04-17 11:56:02 +0800

    Committed on or around 2024/04/17

--- PHP - A Beginner's Guide/Chapter7 - Working with Database and MySQL/Adding Records.sql
+++ PHP - A Beginner's Guide/Chapter7 - Working with Database and MySQL/Adding Records.sql
@@ -1,1 +1,2 @@
-INSERT INTO artists (artist_id, artist_name, artist_country) values ('1', "Aerosmith", "US");
+INSERT INTO artists (artist_id, artist_name, artist_country)
+values ('1', "Aerosmith", "US");
