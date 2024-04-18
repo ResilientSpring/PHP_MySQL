142193a6329e7d1c2a6bbaf2080989394163a6de
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-04-18 16:52:55 +0800

    Committed on or around 2024/04/18

--- W3School SQL Tutorial/INSERT INTO.sql
+++ W3School SQL Tutorial/INSERT INTO.sql
@@ -1,1 +1,2 @@
-INSERT INTO customers()
+INSERT INTO customers(customerName, contactName, address, city, postalCode, country)
+VALUES ("Alfreds Futterkiste", "Maria Anders", "Obere Str. 57", "Berlin", 12209, "Germany");
