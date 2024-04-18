02329d10ca9752cf25cf54166f7a3a2f80693a57
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-04-18 23:06:09 +0800

    Committed on or around 2024/04/18

--- W3School SQL Tutorial/%_ wild cards 2.sql
+++ W3School SQL Tutorial/%_ wild cards 2.sql
@@ -1,2 +1,6 @@
 SELECT * FROM customers
 WHERE customers.customers.contactName LIKE "A%" OR "B___%";
+
+-- If you want to return all columns, use SELECT *  .[1]
+--
+-- Reference: https://www.w3schools.com/sql/sql_select.asp
