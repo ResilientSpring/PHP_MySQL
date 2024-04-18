44b639b0d0d937ae4e27d5c2549a7f2bddb021df
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-04-18 18:04:40 +0800

    Committed on or around 2024/04/18

--- Experimenter/See if PRIMARY KEY already implies NOT NULL 2.sql
+++ Experimenter/See if PRIMARY KEY already implies NOT NULL 2.sql
@@ -1,1 +1,2 @@
-INSERT INTO suppliers3(supplierName, contactName, address, city, postCode, country) VALUES ("Tokyo Traders", "Yoshi Nagase", "9-8 Sekimai", "Tokyo", "100", "Japan");
+INSERT INTO suppliers3(supplierName, contactName, address, city, postCode, country)
+VALUES ("Tokyo Traders", "Yoshi Nagase", "9-8 Sekimai", "Tokyo", "100", "Japan");
