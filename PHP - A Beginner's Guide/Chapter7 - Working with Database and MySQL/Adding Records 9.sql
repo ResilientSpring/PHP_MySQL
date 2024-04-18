3601d3ca4d275b1815993bcd1cbaa0224eee99ec
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-04-18 13:29:28 +0800

    Committed on or around 2024/04/18

---
+++ PHP - A Beginner's Guide/Chapter7 - Working with Database and MySQL/Adding Records 9.sql
@@ -0,0 +1,3 @@
+INSERT INTO songs(song_title, foreign_key_song_artist, foreign_key_song_rating) VALUES ("Aplogize", NULL, 4);
+INSERT INTO songs(song_title, foreign_key_song_artist, foreign_key_song_rating) VALUES ("SOS", NULL, 4);
+INSERT INTO songs(song_title, foreign_key_song_artist, foreign_key_song_rating) VALUES ("Dancing Queen", NULL, 4);
