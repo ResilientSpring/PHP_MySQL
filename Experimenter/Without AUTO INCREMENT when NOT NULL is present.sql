4e47ebbc8e24ae80eb20251f5e04d95b7f42eac1
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-04-18 17:22:14 +0800

    Committed on or around 2024/04/18

--- Experimenter/Without AUTO INCREMENT when NOT NULL is present.sql
+++ Experimenter/Without AUTO INCREMENT when NOT NULL is present.sql
@@ -1,5 +1,12 @@
 CREATE TABLE suppliers(
     supplierID INT NOT NULL PRIMARY KEY ,
     supplierName VARCHAR(255) NOT NULL UNIQUE ,
+    contactName VARCHAR(255) ,
+    address VARCHAR(255),
+    city VARCHAR(255),
+    postCode INT,
+    country VARCHAR(255)
 
 );
+
+
