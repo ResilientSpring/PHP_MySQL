f5dd3337e822413ab9e99496948a3d4621afb498
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-04-18 21:07:15 +0800

    Committed on or around 2024/04/18

---
+++ W3School SQL Tutorial/INSERT INTO 2.sql
@@ -0,0 +1,3 @@
+INSERT INTO customers(customerName, contactName, address, city, postalCode, country)
+VALUES ("Around The Horn", "Thomas Hardy", "120 Hanover Sq.",
+        "London", "WA1 1DP", "UK");
