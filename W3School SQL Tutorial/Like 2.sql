299edcee7d17b399dcf690f3bc414005bf19bc98
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-04-18 22:33:11 +0800

    Committed on or around 2024/04/18

--- W3School SQL Tutorial/Like 2.sql
+++ W3School SQL Tutorial/Like 2.sql
@@ -1,1 +1,6 @@
 SELECT * FROM customers WHERE customers.customers.customerName LIKE "A%";
+
+-- Wild cards operators % and _ can only be used in conjunction with
+-- LIKE . [1]
+--
+-- Reference: https://www.w3schools.com/sql/sql_like.asp
