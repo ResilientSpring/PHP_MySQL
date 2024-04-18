2d8bf0780dff0a3d7844e70eddcba2f23c88eba0
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-04-18 17:28:31 +0800

    Committed on or around 2024/04/18

--- Experimenter/Without AUTO INCREMENT when NOT NULL is present.sql
+++ Experimenter/Without AUTO INCREMENT when NOT NULL is present.sql
@@ -1,12 +1,15 @@
 CREATE TABLE suppliers(
-    supplierID INT NOT NULL PRIMARY KEY ,
+    supplierID INT UNSIGNED NOT NULL PRIMARY KEY ,
     supplierName VARCHAR(255) NOT NULL UNIQUE ,
     contactName VARCHAR(255) ,
     address VARCHAR(255),
     city VARCHAR(255),
-    postCode INT,
+    postCode VARCHAR(255),
     country VARCHAR(255)
 
 );
 
-
+INSERT INTO suppliers(supplierID, supplierName, contactName, address,
+                      city, postCode, country)
+VALUES (1, "Exotic Liquid", "Charlotte Copper", "49 Gilbert St.",
+        "Londona", "EC1 4SD", "UK");
