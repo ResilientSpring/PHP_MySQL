b25aaad496d0bded595b93f67aaa045f2f45727f
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-04-19 19:27:06 +0800

    Committed on or around 2024/04/19

--- PHP - A Beginner's Guide/Chapter7 - Working with Database and MySQL/UPDATE .sql
+++ PHP - A Beginner's Guide/Chapter7 - Working with Database and MySQL/UPDATE .sql
@@ -1,2 +1,5 @@
 UPDATE songs SET foreign_key_song_artist = 4
              WHERE song_title = "Another Crack In My Heart";
+
+UPDATE songs SET foreign_key_song_artist = 3
+             WHERE song_title = "Aplogize";
