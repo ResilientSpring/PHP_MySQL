26228d92628f2c2f8b53783858b95ea38d0455a9
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-04-19 13:48:18 +0800

    Committed on or around 2024/04/19

--- W3School SQL Tutorial/Purpose of FOREIGN KEY.sql
+++ W3School SQL Tutorial/Purpose of FOREIGN KEY.sql
@@ -1,1 +1,7 @@
 INSERT INTO orders(orderNumber, personID) VALUES (0809449, 23);
+
+# FOREIGN KEY prevents invalid data from being inserted into
+# the foreign key column, because it has to be one of the values
+# contained in the parent table's primary key.[1]
+
+# Reference: https://www.w3schools.com/sql/sql_foreignkey.asp
