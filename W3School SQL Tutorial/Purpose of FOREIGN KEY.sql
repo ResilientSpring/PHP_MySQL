6629ed80599a83a5240c3ce05bc4d839a8a8c932
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-04-19 14:06:10 +0800

    Committed on or around 2024/04/19

--- W3School SQL Tutorial/Purpose of FOREIGN KEY.sql
+++ W3School SQL Tutorial/Purpose of FOREIGN KEY.sql
@@ -5,3 +5,6 @@ INSERT INTO orders(orderNumber, personID) VALUES (0809449, 23);
 # contained in the parent table's primary key.[1]
 
 # Reference: https://www.w3schools.com/sql/sql_foreignkey.asp
+
+INSERT INTO orders(orderNumber, personID) VALUES (20806449, 4);
+# In this case, 23 does not exist in the primary key of the table it references.
