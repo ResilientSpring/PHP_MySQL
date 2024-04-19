866ed26981ea408b92188ba71b54ad81743e5ad1
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-04-19 13:40:34 +0800

    Committed on or around 2024/04/19

--- W3School SQL Tutorial/INSERT INTO 5.sql
+++ W3School SQL Tutorial/INSERT INTO 5.sql
@@ -2,6 +2,6 @@ INSERT INTO orders(orderID, orderNumber, personID)
 VALUES (4, 24562, 1);
 
 # Foreign key prevents the entry of data into a table when there is
-# no matching value in the parent table.
+# no matching value in the parent table's primary key.
 INSERT INTO orders(orderID, orderNumber, personID)
 VALUES (5, 67890, 8);
