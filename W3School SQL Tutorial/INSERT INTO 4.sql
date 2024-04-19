8e885699a10822d5acbadaf68c511bdbab2313a3
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-04-19 13:06:08 +0800

    Committed on or around 2024/04/18

--- W3School SQL Tutorial/INSERT INTO 4.sql
+++ W3School SQL Tutorial/INSERT INTO 4.sql
@@ -3,3 +3,6 @@ VALUES (1, "Hansen", "Ola", 30);
 
 INSERT INTO persons(personID, lastName, firstName, age)
 VALUES (2, "Svendson", "Tove", 23);
+
+INSERT INTO persons(lastName, firstName, age)
+VALUES ("Pettersen", "Kari", 20);
