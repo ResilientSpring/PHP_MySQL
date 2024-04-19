a6037756b0fba89c95bd4a0b1e4306102a78855a
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-04-19 19:56:14 +0800

    Committed on or around 2024/04/19

--- Experimenter/ALTER .sql
+++ Experimenter/ALTER .sql
@@ -1,2 +1,5 @@
 ALTER TABLE songs
-    ADD FOREIGN KEY (foreign_key_song_artist) REFERENCES artists(artist_id);
+    ADD FOREIGN KEY (foreign_key_song_artist)
+        REFERENCES artists(artist_id);
+
+# Reference: https://www.w3schools.com/sql/sql_foreignkey.asp
