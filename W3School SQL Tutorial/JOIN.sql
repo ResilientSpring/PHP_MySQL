770ebbe4f829506e03b6ad2f50c44705e7721c1d
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-04-22 16:14:05 +0800

    Committed on or around 2024/04/22

--- W3School SQL Tutorial/JOIN.sql
+++ W3School SQL Tutorial/JOIN.sql
@@ -1,3 +1,10 @@
 SELECT orders.orderID, customers.customerName, orders.orderDate
 FROM orders
 INNER JOIN customers on orders.customerID = customers.customerID;
+
+
+
+
+SELECT orders.orderID, orders.orderDate FROM orders;
+
+SELECT * FROM orders;
