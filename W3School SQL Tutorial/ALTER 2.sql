c44cb7dae52ee467ab47efc09beccfd148825cc8
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-04-22 12:22:36 +0800

    Committed on or around 2024/04/22

--- W3School SQL Tutorial/ALTER 2.sql
+++ W3School SQL Tutorial/ALTER 2.sql
@@ -1,3 +1,6 @@
 ALTER TABLE orders RENAME COLUMN OrderDate TO orderDate;
 
 ALTER TABLE orders CHANGE COLUMN `OrderDate` `orderDate` DATE;
+
+# Reference:
+# 1. https://stackoverflow.com/questions/53735305/how-to-rename-a-column-name-in-maria-db
