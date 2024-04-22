f9d37d45471674c6b1e24ef94fb6b108ba031a6c
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-04-22 12:23:25 +0800

    Committed on or around 2024/04/22

--- W3School SQL Tutorial/ALTER 2.sql
+++ W3School SQL Tutorial/ALTER 2.sql
@@ -1,6 +1,7 @@
-ALTER TABLE orders RENAME COLUMN OrderDate TO orderDate;
+ALTER TABLE orders RENAME COLUMN OrderDate TO orderDate; #[1]
 
-ALTER TABLE orders CHANGE COLUMN `OrderDate` `orderDate` DATE;
+ALTER TABLE orders CHANGE COLUMN `OrderDate` `orderDate` DATE; #[2]
 
 # Reference:
-# 1. https://stackoverflow.com/questions/53735305/how-to-rename-a-column-name-in-maria-db
+# 1. https://www.w3schools.com/sql/sql_alter.asp
+# 2. https://stackoverflow.com/questions/53735305/how-to-rename-a-column-name-in-maria-db
