afd4cc8becc4985d2769900a68574cff8a88ca4d
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-06-10 22:59:32 +0800

    Committed on or around 2024/06/10

--- PHP - A Beginner's Guide/Chapter4 - Working with Arrays/pizza.php
+++ PHP - A Beginner's Guide/Chapter4 - Working with Arrays/pizza.php
@@ -1,1 +1,22 @@
-<?php
+<html>
+
+<head>
+    <title>Project 4-2: Pizza Topping Selector</title>
+</head>
+
+<body>
+
+<h2>Project 4-2: Pizza Topping Selector</h2>
+
+You selected the following toppings:<br/>
+
+<ul>
+    <?php
+    foreach ($_POST['toppings'] as $t)
+        echo "<li>$t</li> \r\n";
+    ?>
+</ul>
+
+</body>
+
+</html>
