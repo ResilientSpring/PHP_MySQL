9aee9012ff2dc068fb5619bfba8c8c2e90be8310
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2026-03-07 00:19:31 +0800

    Committed on or around 2026/03/07

--- W3School SQL Tutorial/SQL Tutorial/SQL Select/Syntax.sql
+++ W3School SQL Tutorial/SQL Tutorial/SQL Select/Syntax.sql
@@ -1,1 +1,8 @@
 SELECT CustomerName, City FROM customers;
+
+-- " FROM table_name, SELECT column1, column2;
+-- is not valid SQL in any standard SQL dialect (MySQL, PostgreSQL,
+-- SQL Server, SQLite, etc.) ". [1]
+
+-- References:
+-- 1.https://chatgpt.com/c/69aaf9c3-3774-8320-af0c-d251e5d529d4
