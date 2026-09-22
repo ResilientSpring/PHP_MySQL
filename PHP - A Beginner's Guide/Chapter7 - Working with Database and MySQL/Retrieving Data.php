650505480225aba2e30e0bd82caafd6322177321
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2026-09-22 16:28:09 +0800

    Committed on or around 2026/09/22

--- PHP - A Beginner's Guide/Chapter7 - Working with Database and MySQL/Retrieving Data.php
+++ PHP - A Beginner's Guide/Chapter7 - Working with Database and MySQL/Retrieving Data.php
@@ -2,4 +2,6 @@
 // attempt database connection
 $mysqli = new mysqli("localhost", "root", null, "music");
 
-
+if($mysqli === false){
+    die("ERROR: Could not connect. ".mysqli_connect_error());
+}
