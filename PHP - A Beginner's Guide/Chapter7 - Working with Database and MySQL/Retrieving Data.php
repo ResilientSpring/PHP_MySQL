914bedf8099d0ab27509e5d7cbac59306d372f7d
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2026-09-22 16:35:13 +0800

    Committed on or around 2026/09/22

--- PHP - A Beginner's Guide/Chapter7 - Working with Database and MySQL/Retrieving Data.php
+++ PHP - A Beginner's Guide/Chapter7 - Working with Database and MySQL/Retrieving Data.php
@@ -5,3 +5,12 @@ $mysqli = new mysqli("localhost", "root", null, "music");
 if($mysqli === false){
     die("ERROR: Could not connect. ".mysqli_connect_error());
 }
+
+// attempt query execution
+// iterate over result set
+// print each record and its fields
+// output: "1:Aerosmith \n 2:Abba \n ..."
+$sql = "SELECT artist_id, artist_name, FROM artists";
+if ($result = $mysqli.query($sql)){
+
+}
