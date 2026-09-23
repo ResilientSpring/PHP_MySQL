7875646577b5dca11244ebb3a0ed250fc4b89bc7
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2026-09-23 14:36:30 +0800

    Committed on or around 2026/09/23

--- Assortment/Pumpkin.php
+++ Assortment/Pumpkin.php
@@ -1,3 +1,11 @@
 <?php
 // attempt database connection
 $mysqli = new mysqli("127.0.0.1", "root", null, "pumpkin");
+
+if($mysqli === false){
+
+    die("ERROR: Could not connect.". mysqli_connect_error());
+
+}
+
+
